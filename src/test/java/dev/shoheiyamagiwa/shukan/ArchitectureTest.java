05fb8fb093cddffbff0b10fe021f3eac9c436ade
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-05-30 21:04:32 +0900

    fix: refactor `ArchitectureTest` to restrict direct access from Repository to Controller

--- src/test/java/dev/shoheiyamagiwa/shukan/ArchitectureTest.java
+++ src/test/java/dev/shoheiyamagiwa/shukan/ArchitectureTest.java
@@ -19,159 +19,154 @@ public final class ArchitectureTest {
 	private static JavaClasses importedClasses;
 	
 	@BeforeAll
-	public static void setUp() {
-		importedClasses =
-				new ClassFileImporter()
-						.withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-						.importPackages("dev.shoheiyamagiwa.shukan");
+	public static void setup() {
+		importedClasses = new ClassFileImporter()
+			.withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+			.importPackages("dev.shoheiyamagiwa.shukan");
 	}
 	
 	@Test
 	public void testLayeredArchitecture() {
 		Architectures.layeredArchitecture()
-				.consideringAllDependencies()
-				.withOptionalLayers(true)
-				.layer("Controller")
-				.definedBy(
-						"dev.shoheiyamagiwa.shukan.presentation..",
-						"dev.shoheiyamagiwa.shukan.middleware..",
-						"dev.shoheiyamagiwa.shukan")
-				.layer("Service")
-				.definedBy("dev.shoheiyamagiwa.shukan.service..")
-				.layer("Repository")
-				.definedBy("dev.shoheiyamagiwa.shukan.infra..")
-				.layer("Entity")
-				.definedBy("dev.shoheiyamagiwa.shukan.domain..")
-				.whereLayer("Controller")
-				.mayNotBeAccessedByAnyLayer()
-				.whereLayer("Service")
-				.mayOnlyBeAccessedByLayers("Controller", "Service")
-				.whereLayer("Repository")
-				.mayOnlyBeAccessedByLayers("Controller", "Service", "Repository")
-				.check(importedClasses);
+			.consideringAllDependencies()
+			.withOptionalLayers(true)
+			.layer("Bootstrap")
+			.definedBy("dev.shoheiyamagiwa.shukan")
+			.layer("Controller")
+			.definedBy("dev.shoheiyamagiwa.shukan.presentation..", "dev.shoheiyamagiwa.shukan.middleware..")
+			.layer("Service")
+			.definedBy("dev.shoheiyamagiwa.shukan.service..")
+			.layer("Repository")
+			.definedBy("dev.shoheiyamagiwa.shukan.infra..")
+			.layer("Entity")
+			.definedBy("dev.shoheiyamagiwa.shukan.domain..")
+			.whereLayer("Bootstrap")
+			.mayNotBeAccessedByAnyLayer()
+			.whereLayer("Controller")
+			.mayOnlyBeAccessedByLayers("Bootstrap")
+			.whereLayer("Service")
+			.mayOnlyBeAccessedByLayers("Bootstrap", "Controller", "Service")
+			.whereLayer("Repository")
+			.mayOnlyBeAccessedByLayers("Bootstrap", "Service", "Repository")
+			.check(importedClasses);
 	}
 	
 	@Test
 	public void testFinalAndNoInheritance() {
-		classes()
-				.that()
-				.areNotInterfaces()
-				.and()
-				.areNotEnums()
-				.and()
-				.areNotRecords()
-				.should()
-				.haveModifier(JavaModifier.FINAL)
-				.andShould(
-						new ArchCondition<>(
-								"not extend any class other than Object, Record, Enum, or Exception") {
-							@Override
-							public void check(JavaClass item, ConditionEvents events) {
-								if (item.getSuperclass().isPresent()) {
-									String superclassName = item.getSuperclass().get().getName();
-									boolean isAllowed =
-											superclassName.equals("java.lang.Object")
-													|| superclassName.equals("java.lang.Record")
-													|| superclassName.equals("java.lang.Enum")
-													|| item.isAssignableTo(Exception.class);
-									if (!isAllowed) {
-										events.add(
-												SimpleConditionEvent.violated(
-														item, item.getName() + " extends " + superclassName));
-									}
-								}
-							}
-						})
-				.allowEmptyShould(true)
-				.check(importedClasses);
+		classes().that()
+			.areNotInterfaces()
+			.and()
+			.areNotEnums()
+			.and()
+			.areNotRecords()
+			.should()
+			.haveModifier(JavaModifier.FINAL)
+			.andShould(new ArchCondition<>("not extend any class other than Object, Record, Enum, or Exception") {
+				@Override
+				public void check(JavaClass item, ConditionEvents events) {
+					if (item.getSuperclass().isPresent()) {
+						String superclassName = item.getSuperclass().get().getName();
+						boolean isAllowed = superclassName.equals("java.lang.Object")
+							|| superclassName.equals("java.lang.Record")
+							|| superclassName.equals("java.lang.Enum")
+							|| item.isAssignableTo(Exception.class);
+						
+						if (!isAllowed) {
+							events.add(SimpleConditionEvent.violated(item, item.getName() + " extends " + superclassName));
+						}
+					}
+				}
+			})
+			.allowEmptyShould(true)
+			.check(importedClasses);
 	}
 	
 	@Test
 	public void testServiceRules() {
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.service..")
-				.and()
-				.areNotInterfaces()
-				.and()
-				.areNotRecords()
-				.should()
-				.haveSimpleNameEndingWith("Service")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.service..")
+			.and()
+			.areNotInterfaces()
+			.and()
+			.areNotRecords()
+			.should()
+			.haveSimpleNameEndingWith("Service")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 	}
 	
 	@Test
 	public void testRepositoryRules() {
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.repository..")
-				.and()
-				.areNotInterfaces()
-				.should()
-				.haveSimpleNameEndingWith("Repository")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.repository..")
+			.and()
+			.areNotInterfaces()
+			.should()
+			.haveSimpleNameEndingWith("Repository")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 		
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.dto..")
-				.should()
-				.haveSimpleNameEndingWith("Dto")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.dto..")
+			.should()
+			.haveSimpleNameEndingWith("Dto")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 		
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.mapper..")
-				.should()
-				.haveSimpleNameEndingWith("Mapper")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.infra.mapper..")
+			.should()
+			.haveSimpleNameEndingWith("Mapper")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 	}
 	
 	@Test
 	public void testControllerRules() {
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.controller..")
-				.should()
-				.haveSimpleNameEndingWith("Controller")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.controller..")
+			.should()
+			.haveSimpleNameEndingWith("Controller")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 		
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.dto..")
-				.should()
-				.haveSimpleNameEndingWith("RequestDto")
-				.orShould()
-				.haveSimpleNameEndingWith("ResponseDto")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.dto..")
+			.should()
+			.haveSimpleNameEndingWith("RequestDto")
+			.orShould()
+			.haveSimpleNameEndingWith("ResponseDto")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 		
 		classes()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.mapper..")
-				.should()
-				.haveSimpleNameEndingWith("Mapper")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.presentation.mapper..")
+			.should()
+			.haveSimpleNameEndingWith("Mapper")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 	}
 	
 	@Test
 	public void testEntityRules() {
 		noClasses()
-				.that()
-				.resideInAPackage("dev.shoheiyamagiwa.shukan.domain..")
-				.should()
-				.dependOnClassesThat()
-				.resideInAnyPackage(
-						"dev.shoheiyamagiwa.shukan.service..",
-						"dev.shoheiyamagiwa.shukan.infra..",
-						"dev.shoheiyamagiwa.shukan.presentation..",
-						"dev.shoheiyamagiwa.shukan.middleware..")
-				.allowEmptyShould(true)
-				.check(importedClasses);
+			.that()
+			.resideInAPackage("dev.shoheiyamagiwa.shukan.domain..")
+			.should()
+			.dependOnClassesThat()
+			.resideInAnyPackage(
+				"dev.shoheiyamagiwa.shukan.service..",
+				"dev.shoheiyamagiwa.shukan.infra..",
+				"dev.shoheiyamagiwa.shukan.presentation..",
+				"dev.shoheiyamagiwa.shukan.middleware..")
+			.allowEmptyShould(true)
+			.check(importedClasses);
 	}
 }
